b70cccf8a679b940912cb40d35e4454a3d4000c7
Nines97 <978922493@qq.com>
2019-11-26 14:59:13 +0800

    mybatis-plus 配置类，添加分页插件

--- src/main/java/cn/nines/scaffold/Application.java
+++ src/main/java/cn/nines/scaffold/Application.java
@@ -1,6 +1,5 @@
 package cn.nines.scaffold;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-@MapperScan("cn.nines.scaffold.sys.mapper")
 public class Application {
 
     public static void main(String[] args){
---
+++ src/main/java/cn/nines/scaffold/config/mybatis/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package cn.nines.scaffold.config.mybatis;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @ClassName: MybatisPlusConfig
+ * @Description: MybatisPlus 配置
+ * @author: Nines
+ * @date: 2019年11月26日 14:39
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("cn.nines.scaffold.sys.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：http://mp.baomidou.com<br>
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+         paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+         paginationInterceptor.setLimit(500);
+        return paginationInterceptor;
+    }
+
+}
