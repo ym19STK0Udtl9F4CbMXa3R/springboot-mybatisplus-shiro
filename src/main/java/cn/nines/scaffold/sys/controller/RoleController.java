2626fdce734103baa92f3b13779abbe1bc196d0e
Nines97 <978922493@qq.com>
2019-11-27 23:23:15 +0800

    添加注释,角色拥有的权限查询和更改方法，角色的冻结与解冻方法

--- src/main/java/cn/nines/scaffold/sys/controller/RoleController.java
+++ src/main/java/cn/nines/scaffold/sys/controller/RoleController.java
@@ -4,9 +4,9 @@ package cn.nines.scaffold.sys.controller;
 import cn.nines.scaffold.common.result.JsonResult;
 import cn.nines.scaffold.common.result.PageRequest;
 import cn.nines.scaffold.sys.entity.Role;
-import cn.nines.scaffold.sys.entity.UserRole;
+import cn.nines.scaffold.sys.entity.RolePermission;
+import cn.nines.scaffold.sys.service.RolePermissionService;
 import cn.nines.scaffold.sys.service.RoleService;
-import cn.nines.scaffold.sys.service.UserRoleService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -28,42 +28,87 @@ public class RoleController {
     private RoleService roleService;
 
     @Resource
-    private UserRoleService userRoleService;
+    private RolePermissionService rolePermissionService;
 
-    @PostMapping("/save")
-    public JsonResult saveUser(@RequestBody Role role){
-        if (role.getId() == null){
-            return roleService.addRole(role) ? JsonResult.success("添加成功") : JsonResult.error("添加失败");
-        }else {
-            return roleService.updateRole(role) ? JsonResult.success("更新成功") : JsonResult.error("更新失败");
-        }
+    /**
+     * 添加角色
+     * @param role 角色
+     * @return JsonResult
+     */
+    @PostMapping("/add")
+    public JsonResult addRole(@RequestBody Role role){
+        return roleService.addRole(role) ? JsonResult.success("添加成功") : JsonResult.error("添加失败");
     }
 
+    /**
+     * 更新角色
+     * @param role 角色
+     * @return JsonResult
+     */
+    @PutMapping("/update")
+    public JsonResult updateRole(@RequestBody Role role){
+        return roleService.updateRole(role) ? JsonResult.success("更新成功") : JsonResult.error("更新失败");
+    }
+
+    /**
+     * 冻结角色
+     * @param id 角色ID
+     * @return JsonResult
+     */
+    @DeleteMapping("/{id}")
+    public JsonResult deleteRole(@PathVariable Long id){
+        return roleService.freezeRoleAndRolePermissionByRoleId(id) ? JsonResult.success("操作成功") : JsonResult.error("操作失败");
+    }
+
+    /**
+     * 恢复角色
+     * @param id 角色ID
+     * @return JsonResult
+     */
     @PutMapping("/{id}")
-    public JsonResult updateStatus(@PathVariable Long id){
-        return roleService.updateStatus(id) ? JsonResult.success("操作成功") : JsonResult.error("操作失败");
+    public JsonResult recoverRole(@PathVariable Long id){
+        return roleService.recoverRoleAndRolePermissionByRoleId(id) ? JsonResult.success("操作成功") : JsonResult.error("操作失败");
     }
 
+    /**
+     * 分页查询
+     * @param page 分页参数
+     * @return JsonResult
+     */
     @PostMapping("/findPage")
     public JsonResult findPage(@RequestBody PageRequest page){
         return JsonResult.success(roleService.findPage(page.getSearchText(), page.getCurrent(), page.getSize()));
     }
 
+    /**
+     * 通过角色ID获取角色详情
+     * @param id 角色ID
+     * @return JsonResult
+     */
     @GetMapping("/{id}")
     public JsonResult findOne(@PathVariable Long id){
         Role role = roleService.getById(id);
         return role == null ? JsonResult.error("获取失败") : JsonResult.success(role);
     }
 
-
-    @GetMapping("/findUserRole/{uid}")
-    public JsonResult findUserRole(@PathVariable Long uid){
-        return JsonResult.success(userRoleService.findListByUid(uid));
+    /**
+     * 通过角色ID获取对应的角色权限表数据
+     * @param rid 角色ID
+     * @return JsonResult1
+     */
+    @GetMapping("/findRolePermission/{rid}")
+    public JsonResult findUserRole(@PathVariable Long rid){
+        return JsonResult.success(rolePermissionService.findListByRid(rid));
     }
 
-    @PostMapping("/modifyUserRole")
-    public JsonResult modifyUserRole(@RequestBody List<UserRole> userRoles){
-        return userRoleService.modifyUserRole(userRoles) ? JsonResult.success("分配成功") : JsonResult.error("分配失败");
+    /**
+     * 修改用户的角色
+     * @param rolePermissions 用户角色信息
+     * @return JsonResult
+     */
+    @PostMapping("/modifyRolePermission")
+    public JsonResult modifyUserRole(@RequestBody List<RolePermission> rolePermissions){
+        return rolePermissionService.modifyUserRole(rolePermissions) ? JsonResult.success("分配成功") : JsonResult.error("分配失败");
     }
 
 }
