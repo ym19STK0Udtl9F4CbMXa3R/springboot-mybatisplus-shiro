63d39551b14f86aec54ddf4a2d67588da14a0409
Nines97 <978922493@qq.com>
2019-11-27 23:05:02 +0800

    添加注释

--- src/main/java/cn/nines/scaffold/sys/controller/PermissionController.java
+++ src/main/java/cn/nines/scaffold/sys/controller/PermissionController.java
@@ -24,20 +24,41 @@ public class PermissionController {
     @Resource
     private PermissionService permissionService;
 
-    @PostMapping("/save")
-    public JsonResult saveUser(@RequestBody Permission permission){
-        if (permission.getId() == null){
-            return permissionService.addPermission(permission) ? JsonResult.success("添加成功") : JsonResult.error("添加失败");
-        }else {
-            return permissionService.updatePermission(permission) ? JsonResult.success("更新成功") : JsonResult.error("更新失败");
-        }
+    /**
+     * 添加权限
+     * @param permission 权限
+     * @return JsonResult
+     */
+    @PostMapping("/add")
+    public JsonResult addPermission(@RequestBody Permission permission){
+        return permissionService.addPermission(permission) ? JsonResult.success("添加成功") : JsonResult.error("添加失败");
     }
 
+    /**
+     * 修改权限
+     * @param permission 权限
+     * @return JsonResult
+     */
+    @PutMapping("/update")
+    public JsonResult updatePermission(@RequestBody Permission permission){
+        return permissionService.updatePermission(permission) ? JsonResult.success("更新成功") : JsonResult.error("更新失败");
+    }
+
+    /**
+     * 分页信息
+     * @param page 分页参数
+     * @return JsonResult
+     */
     @PostMapping("/findPage")
     public JsonResult findPage(@RequestBody PageRequest page){
         return JsonResult.success(permissionService.findPage(page.getSearchText(), page.getCurrent(), page.getSize()));
     }
 
+    /**
+     * 通过ID获取权限详情
+     * @param id 权限ID
+     * @return JsonResult
+     */
     @GetMapping("/{id}")
     public JsonResult findOne(@PathVariable Long id){
         Permission permission = permissionService.getById(id);
